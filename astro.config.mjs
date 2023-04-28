3535cfff78404cc9df5b9bb50fdbe63eb9422a84
roryjshelton <76418221+roryjshelton@users.noreply.github.com>
2023-04-27 17:40:05 -0700

    header -> client:load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import react from '@astrojs/react';
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), react()]
 });
