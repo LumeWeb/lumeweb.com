966fc14ae4433a1f29f4f89e511582b3234415cc
cymon <cymon@mailbox.org>
2023-05-23 21:25:18 +0000

    refactor: revise and restructure css/layout/header/footer/meme/etc (wip)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind({ config: { applyBaseStyles: false } }), image(), react()],
 });
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,54 @@
-import React, {useState} from 'react'
-import './NavbarStyles.css'
+import React, { useState, useEffect } from 'react';
+import './NavbarStyles.css';
+import logo from '../../../src/assets/lume-logo.png';
 
-function Navbar() {
-    const [nav, setNav] = useState(false)
-    const toggleNav = () => setNav(!nav)
+function Navbar({ view }) {
+  const [nav, setNav] = useState(false);
+  const toggleNav = () => setNav(!nav);
 
-    return (
-        <div class='w-full h-20 flex justify-between items-center whitespace-nowrap gap-9 bg-[#031418] pt-[75px] px-[3%]'>
-            <div class="bg-[url(../../src/assets/lume-logo.webp)] bg-no-repeat bg-contain h-[55px] min-w-[210px] z-[998] ml-[1%]">
-            </div>
-            <ul class='transition-opacity duration-150 ease-out md:max-lg:opacity-0 lg:opacity-100 nav-menu'>
-                <li><a href="#">Home</a></li>
-                <li><a href="#vision">Vision</a></li>
-                <li><a href="#whatisweb3">What is Web3</a></li>
-                <li>Donate</li>
-                <div class='text-[color:var(--black)] font-normal not-italic items-end bg-[color:var(--aquamarine)] flex h-[52px] max-w-[195px] transition-[0.5s] border px-[5.2px] py-3 rounded-[5px] border-solid border-transparent scale-100 hover:transition-[0.5s] hover:bg-transparent hover:border hover:border-[color:var(--aquamarine)] hover:text-[white] hover:border-solid;'>
-                    <a class='download-btn' href="#">Download Extension</a>
-                </div>
-            </ul>
+  useEffect(() => {
+    let resizeTimeout;
+    const onResize = () => {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(() => {
+        if(nav && window.innerWidth > 940) {
+          setNav(false);
+        }
+      }, 25);
+    };
 
-            <div class="transition-opacity duration-150 ease-out nav-menu hamburger" onClick={toggleNav}>
-            ☰
-            </div>
-            <div class={nav ? 'mobile-menu active class="transition duration-[0.55s] linear"' : 'mobile-menu class="transition duration-[0.55s] linear"'}>
-                <ul class="mobile-nav">
-                    <li onClick={toggleNav}><a href="#">Home</a></li>
-                    <li onClick={toggleNav}><a href="#vision">Vision</a></li>
-                    <li onClick={toggleNav}><a href="#whatisweb3">What is Web3</a></li>
-                    <li onClick={toggleNav}><a href="#donate">Donate</a></li>
-                    <div class='text-[color:var(--black)] text-[length:var(--font-size-s)] font-normal not-italic items-end bg-[color:var(--aquamarine)] flex h-[52px] min-w-[245px] transition-[0.5s] border px-[5.2px] py-3 rounded-[5px] border-solid border-transparent scale-100 hover:transition-[0.5s] hover:bg-transparent hover:border hover:border-[color:var(--aquamarine)] hover:text-[white] hover:border-solid transition-[0.5s] ml-[-0.8em] scale-[0.7] hover:scale-75'>
-                        <a class="download-btn pl-2.5" href="#">Download Extension</a>
-                    </div>
-                </ul>
-                <div class="mobile-menu-btn"></div>
-            </div>
-        </div>
-    )
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, [nav]);
+
+  return (
+    <nav>
+      <div className="logo">
+        <a href="/">
+          <img src={logo.src} alt="Lume" />
+        </a>
+      </div>
+      <menu className={nav ? 'active' : undefined}>
+        <li>
+          <a href="/" className={view === 'home' ? 'underline' : undefined} aria-current={view === 'home' ? 'page' : undefined}>Home</a>
+        </li>
+        <li>
+          <a href="/vision" className={view === 'vision' ? 'underline' : undefined} aria-current={view === 'vision' ? 'page' : undefined}>Vision</a>
+        </li>
+        <li>
+          <a href="/what-is-web3" className={view === 'what-is-web3' ? 'underline' : undefined} aria-current={view === 'what-is-web3' ? 'page' : undefined}>What is Web3</a>
+        </li>
+        <li>
+          <a href="/donate" className={view === 'donate' ? 'underline' : undefined} aria-current={view === 'donate' ? 'page' : undefined}>Donate</a>
+        </li>
+        <li>
+          <a className='btn-main download-btn' href="#">Download Extension</a>
+        </li>
+      </menu>
+      <button className="hamburger" onClick={toggleNav}>☰</button>
+    </nav>
+  )
 }
 
 export default Navbar
