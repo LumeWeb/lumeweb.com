63e0c57b0329f76609f6d9a55a37585dfce7f45d
cymon <cymon@mailbox.org>
2023-06-01 22:15:49 +0000

    feat: svg icons, donation methods

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.scss';
 import logo from '../../../../src/assets/lume-logo.png';
 
