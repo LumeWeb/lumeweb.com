a1b998e4b941abf8557e08e8a3a1affb164cbf59
Juan Di Toro <ditorojuanf@gmail.com>
2023-08-14 16:13:11 +0200

    fix: gridcell style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  variants: {
+    extend: {
+      boxShadow: ['active']
+    }
+  },
   theme: {
     extend: {
       colors: {
         primary: 'var(--color-aquamarine)',
         "dark-aquamarine": 'var(--color-dark-aquamarine)',
         body: 'var(--color-gray)',
-      },
+        "gradient-aqua": "linear-gradient(to right, var(--color-dark-aquamarine), var(--color-aquamarine))",
+      }
     },
     fontFamily: {
       'body': '"Jaldi", sans-serif',
       'display': '"JetBrains Mono", monospace',
       'display2': '"IBM Plex Sans Devanagari", monospace',
-    }
-  }
+    },
+  },
 }
