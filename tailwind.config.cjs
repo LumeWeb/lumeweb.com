088f7935bdd9fff6a796e9d4d43dc41ade0d6208
Juan Di Toro <ditorojuanf@gmail.com>
2023-08-15 17:07:26 +0200

    refactor: '/donate' page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  variants: {
-    extend: {
-      boxShadow: ['active']
-    }
-  },
+  important: false,
   theme: {
     extend: {
       colors: {
