599bba5c352ead5981732d68a5c83cc42a40cb65
Derrick Hammer <derrick@derrickhammer.com>
2023-08-18 22:38:30 -0400

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({ config: { applyBaseStyles: false } }), react()],
+    site: 'https://lumeweb.com',
+    integrations: [tailwind({ config: { applyBaseStyles: false } }), react(), sitemap()],
     experimental: {
         assets: true
     }
